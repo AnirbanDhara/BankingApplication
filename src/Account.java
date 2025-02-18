51dc8620d90e01be7bd7cae0c337f6101727ba36
anirban <anirbandhara1997@gmail.com>
2025-02-18 22:36:23 +0530

    Account (Abstract class)

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+/* abstract class declared with default previliges. It can only be accessed
+ from within the package. This class has two abstract methods */
 
-public class Account {
+abstract class Account {
+    /* balance within the account */
+    float balance;
+
+    /* Account No */
+    private String accountNo;
+
+    /* constructor to initialize balance and account no */
+    Account(float b, String acc){
+        balance = b;
+        accountNo = acc;
+    }
+    /* getter method to access balance */
+    float getBalance()
+    {
+        return balance;
+    }
+
+    /* setter method to modify balance */
+    void setBalance(float b)
+    {
+        balance = b;
+    }
+
+    /* The methods are declared abstract so that the subclasses can code them according
+    to their respective needs keeping their names in tact */
+
+    abstract void debit(float amount);
+    abstract void credit(float amount);
+
+    /* getter method to access account no */
+    String getAccountNo()
+    {
+        return accountNo;
+    }
+
+    /* setter method to modify balance */
+    void setAccountNo(String acc)
+    {
+        accountNo = acc;
+    }
+
+    /* Print Account No and balance details */
+    void display()
+    {
+        System.out.println("Account Number: "+ accountNo);
+        System.out.println("Account Balance: "+balance);
+    }
+
 }
